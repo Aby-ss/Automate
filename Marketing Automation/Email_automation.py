7cd8a84e90ce2e3fc097baf2c20523131087baea
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-16 11:37:41 +0400

    Added new base logic for MailGun API with errror handling ✅

--- Marketing Automation/Email_automation.py
+++ Marketing Automation/Email_automation.py
@@ -4,36 +4,23 @@ from rich.panel import Panel
 
 # Mailgun API endpoint and credentials
 MAILGUN_API_KEY = '262b213e-b924eb47'
-MAILGUN_DOMAIN = 'your_mailgun_domain'
+MAILGUN_DOMAIN = 'sandbox767a73cadd3d4ccdbca30676ce7937f7.mailgun.org'
 MAILGUN_API_URL = f'https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages'
 
-# Email content
-subject = 'Hello from Python!'
-body = 'This is the body of the email.'
-
-# Sender and recipient details
-sender_email = 'your_email@example.com'
-recipient_emails = ['recipient1@example.com', 'recipient2@example.com']
-
-# Send the email
-def send_email(sender, recipients, subject, body):
-    request_data = {
-        'from': sender,
-        'to': recipients,
-        'subject': subject,
-        'text': body
-    }
-    response = requests.post(
-        MAILGUN_API_URL,
-        auth=('api', MAILGUN_API_KEY),
-        data=request_data
-    )
+def send_simple_message():
+    return requests.post(
+        "https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
+        auth=("api", "262b213e-b924eb47"),
+        data={"from": "Rao <mailgun@sandbox767a73cadd3d4ccdbca30676ce7937f7.mailgun.org>",
+              "to": ["raoabdulhadi952@gmail.com", "rao@sandbox767a73cadd3d4ccdbca30676ce7937f7.mailgun.org"],
+              "subject": "Hello",
+              "text": "Testing some Mailgun awesomness!"})
+    
     if response.status_code == 200:
         success_panel = Panel('[bold green]Email sent successfully![/]', style='green')
         print(success_panel)
     else:
-        failure_panel = Panel(f'[bold red]Failed to send email. Response:[/]\n{response.text}', style='red')
+        failure_panel = Panel(f'[bold red]Failed to send email. Response:[/]\n{response.status_code} - {response.content}', style='red')
         print(failure_panel)
-
-# Call the send_email function
-send_email(sender_email, recipient_emails, subject, body)
+    
+send_simple_message()
