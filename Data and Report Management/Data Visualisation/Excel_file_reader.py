9e339ab2b5f08a580b33a795e4709f9c8e6e9339
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-19 02:31:43 +0400

    Updated File scanner base code ✅

--- Data and Report Management/Data Visualisation/Excel_file_reader.py
+++ Data and Report Management/Data Visualisation/Excel_file_reader.py
@@ -1,18 +1,11 @@
 import pandas as pd
+from tabulate import tabulate
 
-# Define the path to your Excel file
-excel_file_path = 'path/to/your/excel/file.xlsx'
+# Read the Excel file into a DataFrame
+df = pd.read_excel('Examplery_data.xlsx')
 
-# Load the Excel file into a pandas DataFrame
-data_frame = pd.read_excel(excel_file_path)
+# Convert DataFrame to rich table format
+table = tabulate(df, headers='keys', tablefmt='fancy_grid')
 
-# Extract data from specific sheet
-sheet_name = 'Sheet1'  # Replace with your sheet name
-sheet_data = data_frame.parse(sheet_name)
-
-# Extract specific columns from the sheet
-columns_to_extract = ['Column1', 'Column2', 'Column3']  # Replace with your column names
-extracted_data = sheet_data[columns_to_extract]
-
-# Print the extracted data
-print(extracted_data)
+# Print the rich table
+print(table)
