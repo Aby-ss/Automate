85c300bad23338ba394c89127e31930550b4700f
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-19 02:43:51 +0400

    Fixed Cell scan error ✅

--- Data and Report Management/Data Visualisation/Excel_file_reader.py
+++ Data and Report Management/Data Visualisation/Excel_file_reader.py
@@ -1,11 +1,21 @@
-import pandas as pd
+import openpyxl
 from tabulate import tabulate
 
-# Read the Excel file into a DataFrame
-df = pd.read_excel('Examplery_data.xlsx')
+from rich.traceback import install
+install(show_locals=True)
 
-# Convert DataFrame to rich table format
-table = tabulate(df, headers='keys', tablefmt='fancy_grid')
+# Load the Excel file
+workbook = openpyxl.load_workbook('Examplery_data.xlsx')
 
-# Print the rich table
-print(table)
+# Select the active sheet
+sheet = workbook.active
+
+# Function to get the value of a specific cell based on its cell number
+def get_cell_value(cell_number):
+    cell = sheet[cell_number]
+    return cell.value
+
+# Example usage: retrieve the value of cell 'A1'
+cell_number = 'C7'
+cell_value = get_cell_value(cell_number)
+print(f"The value of cell {cell_number} is: {cell_value}")
