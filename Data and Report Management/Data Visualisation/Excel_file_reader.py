fe700b19c70379d5423a6f659a415b28b7edc066
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-20 16:38:02 +0400

    Updated File scanner for Gross profit detials ✅

--- Data and Report Management/Data Visualisation/Excel_file_reader.py
+++ Data and Report Management/Data Visualisation/Excel_file_reader.py
@@ -10,12 +10,30 @@ workbook = openpyxl.load_workbook('Examplery_data.xlsx')
 # Select the active sheet
 sheet = workbook.active
 
-# Function to get the value of a specific cell based on its cell number
-def get_cell_value(cell_number):
-    cell = sheet[cell_number]
-    return cell.value
 
-# Example usage: retrieve the value of cell 'A1'
-cell_number = 'C7'
-cell_value = get_cell_value(cell_number)
-print(f"The value of cell {cell_number} is: {cell_value}")
+# Function to print the cell value of all months
+def gross_profit_values():
+    # Define the cell numbers for each month
+    GROSS_PROFIT_month_cells = {
+        'Jan': 'C6',
+        'Feb': 'D6',
+        'Mar': 'E6',
+        'Apr': 'G6',
+        'May': 'H6',
+        'Jun': 'I6',
+        'Jul': 'K6',
+        'Aug': 'L6',
+        'Sep': 'M6',
+        'Oct': 'O6',
+        'Nov': 'P6',
+        'Dec': 'Q6'
+    }
+    
+    for month, cell_number in GROSS_PROFIT_month_cells.items():
+        cell_value = sheet[cell_number].value
+        print(f"{month} = {cell_number}")
+        print(f"Data: {cell_value}")
+        print()  # Empty line for better readability between months
+
+# Call the function to print the cell values of all months
+gross_profit_values()
