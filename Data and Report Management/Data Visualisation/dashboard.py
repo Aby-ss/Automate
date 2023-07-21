9af8ecdb3fd37fc6441b7181538be16364474b3f
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-07-22 02:43:34 +0400

    Added base code for layout ✅

---
+++ Data and Report Management/Data Visualisation/dashboard.py
@@ -0,0 +1,44 @@
+from rich import print
+from rich import box
+from rich.tree import Tree
+from rich.text import Text
+from rich.align import Align
+from rich.panel import Panel
+from rich.layout import Layout 
+from rich.table import Table
+
+from rich.live import Live
+from rich.prompt import Prompt
+from rich.progress import track
+from rich.progress import BarColumn, Progress, SpinnerColumn, TextColumn
+
+from rich.traceback import install
+install(show_locals=True)
+
+layout = Layout()
+
+layout.split_column(
+    Layout(name = "Header", size=3),
+    Layout(name = "Body"),
+    Layout(name = "Footer", size=3)
+)
+
+class Header:
+
+    def __rich__(self) -> Panel:
+        grid = Table.grid(expand=True)
+        grid.add_column(justify="left")
+        grid.add_column(justify="center", ratio=1)
+        grid.add_column(justify="right")
+        grid.add_row(
+            "📝", "[b]Automate[/] - [i]Data Dashboard[/]", datetime.now().ctime().replace(":", "[blink]:[/]"),
+        )
+        return Panel(grid, style="bold white")
+    
+class Footer:
+
+    def __rich__(self) -> Panel:
+        grid = Table.grid(expand=True)
+        grid.add_column(justify="center", ratio=1)
+        grid.add_row("[i]Empowering Growth through Intelligent Automation[/]")
+        return Panel(grid, style="white on black")
