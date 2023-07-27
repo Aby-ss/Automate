e3d42ae63e23b9e801f9aec1fe564db1bc91139f
Rao <103417697+Aby-ss@users.noreply.github.com>
2023-07-27 18:34:10 +0500

    Update Email_automation.py

--- Marketing Automation/Email_automation.py
+++ Marketing Automation/Email_automation.py
@@ -3,6 +3,7 @@ from rich import print
 from rich.panel import Panel
 
 
+
 def send_simple_message():
 	return requests.post(
 		"https://api.mailgun.net/v3/sandbox767a73cadd3d4ccdbca30676ce7937f7.mailgun.org/messages",
